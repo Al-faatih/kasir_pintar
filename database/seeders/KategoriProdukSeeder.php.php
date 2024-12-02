90654131aee895bab80d1ea58f76842e5d3c95e6
Al-faatih <alfaatih566@gmail.com>
2024-12-02 13:36:44 +0700

    first commit

---
+++ database/seeders/KategoriProdukSeeder.php.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\KategoriProduk;
+use Illuminate\Database\Seeder;
+
+class KategoriProdukSeeder extends Seeder
+{
+    public function run()
+    {
+        // Menambahkan data kategori produk hanya jika belum ada
+        KategoriProduk::firstOrCreate(
+            ['id_kategori' => 'K001'], // Cek id_kategori
+            ['nama_kategori' => 'Aksesoris'] // Data yang akan dimasukkan jika id_kategori tidak ada
+        );
+
+        KategoriProduk::firstOrCreate(
+            ['id_kategori' => 'K002'],
+            ['nama_kategori' => 'Fashion']
+        );
+    }
+}
